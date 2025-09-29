fa07167961e9d3a6a9a445ae6c60791c1795e396
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-09-29 20:09:26 +0300

    update: enhance navbar styling

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -6,10 +6,10 @@ import OpenIcon from "./icons/OpenIcon";
 export default function NavBar() {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="mx-auto flex max-w-[110rem] items-center justify-between border-b-2 border-black p-4">
+    <nav className="mx-auto flex max-w-[110rem] items-center justify-between border-b-1 border-black p-4">
       <Logo />
       <div
-        className={`sm:text[1rem] absolute top-[5.6rem] mx-auto ${open ? "flex" : "hidden"} w-[100vw] h-[100vh] flex-col items-center border-t-2 border-black bg-white p-2 pt-5 text-[2rem] sm:h-auto sm:flex sm:static sm:mx-0 sm:w-auto sm:border-none sm:p-0`}
+        className={`absolute top-[5.6rem] mx-auto text-[2rem] ${open ? "flex" : "hidden"} sm:text-[1.5rem] h-[100vh] w-[100vw] flex-col items-center border-t-2 border-black bg-white p-2 pt-5 sm:static sm:mx-0 sm:flex sm:h-auto sm:w-auto sm:border-none sm:p-0`}
       >
         <ul className="flex flex-col gap-5 sm:flex-row">
           <li>Home</li>
