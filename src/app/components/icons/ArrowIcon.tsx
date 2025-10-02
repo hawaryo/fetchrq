ec05167663f6dcf6ff50b12a5fb8646a1d003e37
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-10-02 21:50:11 +0300

    fix(style): fix arrow rotation did not work

--- src/app/components/icons/ArrowIcon.tsx
+++ src/app/components/icons/ArrowIcon.tsx
@@ -7,9 +7,9 @@ export default function ArrowIcon({ rotate, isdisabled }: Props) {
   return (
     <>
       {/* prettier-ignore */}
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 46 46" width={60} height={60} className={`rotate-${rotate}`}>
-        <circle cx="23.5" cy="23.5" r="22.5" fill={isdisabled?"#D9D9D9":"#A1FF94"} />
-         <path fill="#000" d="M34.9608 24.2937c.6535-.6535.6535-1.7148 0-2.3683l-8.365-8.365c-.6535-.6535-1.7148-.6535-2.3683 0-.6535.6536-.6535 1.7149 0 2.3684l5.5104 5.5104H10.3573c-.92536 0-1.67297.7476-1.67297 1.673s.74761 1.673 1.67297 1.673h19.3806l-5.5104 5.5104c-.6535.6535-.6535 1.7148 0 2.3683s1.7148.6535 2.3683 0l8.365-8.3649v-.0053Z"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" fill="none" style={{transform:`rotate(${rotate}deg)`}}>
+        <circle cx="25.371" cy="25.125" r="25"  fill={isdisabled?"#D9D9D9":"#A1FF94"} />
+        <path d="M39.697 26.438a1.86 1.86 0 0 0 0-2.631l-9.294-9.294a1.86 1.86 0 1 0-2.631 2.632l6.123 6.123H12.36c-1.028 0-1.859.831-1.859 1.859s.831 1.859 1.859 1.859h21.534l-6.123 6.123a1.86 1.86 0 1 0 2.631 2.631l9.294-9.294v-.006z" fill="#000" />
       </svg>
     </>
   );
